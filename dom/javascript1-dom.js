a30999b105dbdc7352ca67ab756a894bfa6482c2
peter <gudqo1995@naver.com>
2020-09-01 14:36:20 +0900

    selector API level 1

--- dom/javascript1-dom.js
+++ dom/javascript1-dom.js
@@ -1,3 +1,18 @@
+//Ex3: Selectors API Level1
+window.addEventListener("load", function(){
+    var section3 = document.getElementById("section3");
+    var txtX = section3.querySelector("input[name='x']");
+    var txtY = section3.querySelector(".txt-y");
+    var btnAdd = section3.querySelector(".btn-add");
+    var txtSum = section3.querySelector(".txt-sum");
+
+    btnAdd.onclick = function () {
+        var x = parseInt(txtX.value);
+        var y = parseInt(txtY.value);
+
+        txtSum.value = x+y;
+    };
+});
 //Ex2: 엘리먼트 선택방법 개선하기
 window.addEventListener("load", function(){
     var section2 = document.getElementById("section2");
