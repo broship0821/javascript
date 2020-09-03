a9d2a004cd41af118dcfb4980f425956320dea01
peter <gudqo1995@naver.com>
2020-09-03 14:27:30 +0900

    children

--- dom/javascript1-dom.js
+++ dom/javascript1-dom.js
@@ -1,3 +1,14 @@
+//Ex4: ChildeNodes를 이용한 노드 선택
+window.addEventListener("load", function(){
+    var section4 = document.querySelector("#section4");
+    var box = section4.querySelector(".box");
+
+    var input1 = box.children[0]; //.childNodes[0];
+    var input2 = box.children[1];
+
+    input1.value = "hello";
+    input2.value = "okay";
+});
 //Ex3: Selectors API Level1
 window.addEventListener("load", function(){
     var section3 = document.getElementById("section3");
