162fe0d35c91c040247b8a6d6ffe343d6c267ea9
peter <gudqo1995@naver.com>
2020-09-07 13:24:31 +0900

    css 속성 변경

--- dom/javascript1-dom.js
+++ dom/javascript1-dom.js
@@ -1,14 +1,18 @@
-//Ex5: 엘리먼트 노드의 속성 변경
+//Ex5: 엘리먼트 노드의 속성 & CSS 속성 변경
 window.addEventListener("load", function(){
     var section = document.querySelector("#section5");
     var srcInput = section.querySelector(".src-input");
     var imgSelect = section.querySelector(".img-select");
     var changeButton = section.querySelector(".change-button");
     var img = section.querySelector(".img");
+    var colorInput = section.querySelector(".color-input");
 
     changeButton.onclick = function(){
         img.src = "imgs/" + srcInput.value;
         //img.src = "imgs/" + imgSelect.value;
+
+        //img.style["border-color"] = colorInput.value;
+        img.style.borderColor = colorInput.value;
     }
 });
 //Ex4: ChildeNodes를 이용한 노드 선택
