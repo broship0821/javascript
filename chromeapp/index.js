586094437c97dc808617fbb070f71810bbc3abed
peter <gudqo1995@naver.com>
2020-12-27 17:37:24 +0900

    dom

--- chromeapp/index.js
+++ chromeapp/index.js
@@ -1,6 +1,15 @@
 //html에 접근하는 방법
 //DOM: document object module
-console.log(document); //html 전체 가져오기
-const title = document.getElementById("title");
-console.log(title);
-title.innerHTML="난 자바스크립트!";
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener("click",handleClick);
+}
+init();
